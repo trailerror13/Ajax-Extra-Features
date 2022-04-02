c3985ec86f69946f332f4d7344ce5d8121e589ff
Bastinjoeid <102047556+Bastinjoeid@users.noreply.github.com>
2022-04-02 02:58:51 -0400

    Update Script.py

--- Script.py
+++ Script.py
@@ -58,10 +58,9 @@ _𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐌𝐞 𝐖𝐢𝐭𝐡 𝐘𝐨𝐮𝐫 𝐂�
 ⪼ 𝐄𝐱𝐚𝐦𝐩𝐥𝐞 ›
 
 <code>/batch https://t.me/MWUpdatez/3 https://t.me/MWUpdatez/8</code>
-𝙲𝚁𝙴𝙳𝙸𝚃𝚂 ›› <a href=https://t.me/MWUpdatez><b>𝙼𝚆-𝚄𝙿𝙳𝙰𝚃𝙴𝚉</b></a>"""                     
-    
- WHOIS_TXT ="""<b>WHOIS MODULE</b>  
- Note:- Give a user details 
+𝙲𝚁𝙴𝙳𝙸𝚃𝚂 ›› <a href=https://t.me/MWUpdatez><b>𝙼𝚆-𝚄𝙿𝙳𝙰𝚃𝙴𝚉</b></a>"""
+    WHOIS_TXT ="""<b>WHOIS MODULE</b>
+Note:- Give a user details
 •/whois :-give a user full details"""
 
     FUN_TXT ="""<b>Gᴀᴍᴇs</b> 
